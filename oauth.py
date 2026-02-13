3b3ae6a09903d6acd5a1bdea7f6729a15b8fcd44
xptensorx <codedarkhorse1899@gmail.com>
2026-02-13 05:14:15 +0000

    feat: get profile using oauth token

--- oauth.py
+++ oauth.py
@@ -1,8 +1,10 @@
 import requests
 
+from config import CONFIG
+
 url = "https://www.freelancer.com/api/users/0.1/self/"
 
-headers = {'freelancer-oauth-v1': '<oauth_access_token>'}
+headers = {'freelancer-oauth-v1': CONFIG['oauth_access_token']}
 
 response = requests.request("GET", url, headers=headers)
 
